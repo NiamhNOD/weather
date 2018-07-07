b410879106c1534392df5f3a52d4975c55027c1c
NiamhNOD <niamhnodonovan@gmail.com>
2018-07-07 14:11:23 +0100

    Now accesses current info from Darksky

--- main.py
+++ main.py
@@ -1,58 +1,54 @@
 import requests
 import json
-import datetime
 import time
 import sqlite3
 import configparser
 
+# Open the config file to read in the Darksky key
 config = configparser.ConfigParser()
 config.read('config.ini')
-
 darkskyKey = config['darksky']['darkskyKey']
 
-currentTime = datetime.datetime.now()
+# Get the time for the API request from Darkysky
 seconds = time.time()
-seconds = int(seconds)
-secondsStr = str(seconds)
-weather_cork_response = requests.get('https://api.darksky.net/forecast/' + darkskyKey + '/51.8498,-8.3959,' + secondsStr + '?exclude=currently,minutely,daily,alerts,flags&units=uk2')
-
+# Request the JSON from Darksky
+weather_cork_response = requests.get('https://api.darksky.net/forecast/' + darkskyKey + '/51.8498,-8.3959,' + str(int(seconds)) + '?exclude=hourly,minutely,daily,alerts,flags&units=uk2')
 weather_cork_json = weather_cork_response.json()
 
-time = seconds
-
+# Loop over the Darksky JSON getting the individual data
 for i in weather_cork_json:
-    print('in json')
-    if i == 'hourly':
-        print('in hourly')
+    if i == 'currently':
         for j in weather_cork_json[i]:
             if j == 'summary':
                 summary = weather_cork_json[i][j]
-            if j == 'icon':
+            elif j == 'icon':
                 icon = weather_cork_json[i][j]
-            if j == 'data':
-                for l in weather_cork_json[i][j][0]:
-                    if l == 'precipIntensity':
-                        precipIn = weather_cork_json[i][j][0][l]
-                    elif l == 'precipProbability':
-                        precipProb = weather_cork_json[i][j][0][l]
-                    elif l == 'temperature':
-                        temp = weather_cork_json[i][j][0][l]
-                    elif l == 'apparentTemperature':
-                        appTemp = weather_cork_json[i][j][0][l]
-                    elif l == 'humidity':
-                        hum = weather_cork_json[i][j][0][l]
-                    elif l == 'pressure':
-                        press = weather_cork_json[i][j][0][l]
-                    elif l == 'windSpeed':
-                        windS = weather_cork_json[i][j][0][l]
-                    elif l == 'windBearing':
-                        windB = weather_cork_json[i][j][0][l]
-                    elif l == 'cloudCover':
-                        cloudC = weather_cork_json[i][j][0][l]
+            elif j == 'precipIntensity':
+                precipIn = weather_cork_json[i][j]
+            elif j == 'precipProbability':
+                precipProb = weather_cork_json[i][j]
+            elif j == 'temperature':
+                temp = weather_cork_json[i][j]
+            elif j == 'apparentTemperature':
+                appTemp = weather_cork_json[i][j]
+            elif j == 'humidity':
+                hum = weather_cork_json[i][j]
+            elif j == 'pressure':
+                press = weather_cork_json[i][j]
+            elif j == 'windSpeed':
+                windS = weather_cork_json[i][j]
+            elif j == 'windBearing':
+                windB = weather_cork_json[i][j]
+            elif j == 'cloudCover':
+                cloudC = weather_cork_json[i][j]
 
+# Connect to the Database
 conn = sqlite3.connect('weathercork.db')
 c = conn.cursor()
-c.execute("INSERT INTO weather VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", (time, summary, icon, precipIn, precipProb, temp, appTemp, hum, press, windS, windB, cloudC))
+
+# Execute and commit that database query
+c.execute("INSERT INTO weather VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", (int(seconds), summary, icon, precipIn, precipProb, temp, appTemp, hum, press, windS, windB, cloudC))
 conn.commit()
 conn.close()
-print(datetime.datetime.fromtimestamp(seconds).strftime('%Y-%m-%D - %H:%M'))
+
+print('The current temperature is ' + str(temp) + 'C. It feels like ' + str(appTemp) + "C.")
